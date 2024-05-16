06745c553b5a02962c6ad38a21a8e43c7d3cf45f
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-05-16 03:48:16 +0200

    Init commit

---
+++ demo-mobile-ios/demo_mobile_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_mobile_iosApp.swift
+//  demo-mobile-ios
+//
+//  Created by Velimir Đurković on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct demo_mobile_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demo-mobile-iosUITests/demo_mobile_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  demo_mobile_iosUITestsLaunchTests.swift
+//  demo-mobile-iosUITests
+//
+//  Created by Velimir Đurković on 5/16/24.
+//
+
+import XCTest
+
+final class demo_mobile_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
